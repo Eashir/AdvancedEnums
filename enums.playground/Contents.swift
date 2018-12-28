3cbbff8811223bed8e1ac863aceddee677d1e2f5
Eashir <eashir.arafat@gmail.com>
2018-12-27 20:11:54 -0500

    healthbar protocol

--- enums.playground/Contents.swift
+++ enums.playground/Contents.swift
@@ -1,1 +1,28 @@
 
+protocol HealthBar {
+	var numberOfHearts: Int { get }
+	
+	mutating func increaseHP()
+	mutating func reduceHP()
+}
+
+enum Player: HealthBar {
+	case dead
+	case alive(currentHealth: Int)
+	
+	var numberOfHearts: Int {
+		switch self {
+		case .dead:
+			return 0
+		case let .alive(numberOfHearts):
+			return numberOfHearts
+		}
+	}
+	
+	mutating func increaseHP() {
+	}
+	
+	mutating func reduceHP() {
+		
+	}
+}
